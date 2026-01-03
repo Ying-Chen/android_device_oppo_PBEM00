f1a6f3868335b565c8a5b72e5e450469ebbb0489
WenHao2130 <wenhao2130@outlook.com>
2026-01-03 14:31:38 +0800

    PBEM00: Build android.hardware.sensors@1.0 hal
    - Make sensors happy
    
    Change-Id: I29430e1acbc0444bc15fac66469cce6d802887a3
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -345,12 +345,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    libdumpstateutil.vendor
-
-PRODUCT_PACKAGES += \
-    libsensorndkbridge
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
