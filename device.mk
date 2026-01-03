d89b940e7f6d382afe09c80a1f41feabca6e6a8d
WenHao2130 <wenhao2130@outlook.com>
2026-01-03 18:46:38 +0800

    PBEM00: Build system variant hidl libs
    - F linker  : CANNOT LINK EXECUTABLE "/system/system_ext/bin/dpmd": library "libhwbinder.so" not found: needed by main executable
    
    Change-Id: Iedcb415027bbf890a90c0061092812df2b8995bc
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    libhidltransport \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # Health
