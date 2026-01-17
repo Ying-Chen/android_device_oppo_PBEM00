0a596617bc8e46979d1f177ded1894c4dfd4de2a
Ying-Chen <yingchen233@outlook.com>
2026-01-17 21:10:12 +0800

    Bump keymaster version to 4
    Using keymaster 4 from Reno - RKQ1.201217.002

--- device.mk
+++ device.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@4.1.vendor
 
 # Light
 PRODUCT_PACKAGES += \
